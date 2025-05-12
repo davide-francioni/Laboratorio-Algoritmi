7c58d7cde532b00211f8b1e0ada38712e770ab1b
davide-francioni <davide.francioni@edu.unifi.it>
2025-05-12 23:38:54 +0200

    update heap

--- Esercizio 1/heap-list.py
+++ Esercizio 1/heap-list.py
@@ -1,4 +1,4 @@
-class PriorityQueueHeap:
+class MaxHeap:
     def __init__(self):
         self.heap = []
 
@@ -12,42 +12,29 @@ class PriorityQueueHeap:
             max_node = left
         else:
             max_node = index
-        if right < len(self.heap) and self.heap[left] < self.heap[index]:
+        if right < len(self.heap) and self.heap[right] < self.heap[index]:
             max_node=right
         if max_node != index:
             self.max_heapify(max_node)
 
     def increase_value(self, index, value):
         if value < self.heap[index]:
-
+            raise ValueError(f"{value} is lower than {value}")
         self.heap[index] = value
-        self.max_heapify(index)
+        while index > 0 and self.heap[(index//2)-1] < self.heap[index]:
+            app=self.heap[(index//2)-1]
+            self.heap[index//2-1]=self.heap[index]
+            self.heap[index]=app
+            index=index//2-1
 
     def insert(self, value):
-        self.heap[len(self.heap)-1]=value
-        self.max_heapify(len(self.heap) - 1)
-
-    def find_min(self):
-        if self.is_empty():
-            return None
-        index = len(self.heap)//2+1
-        min_node = self.heap[index]
-        while index+1 < len(self.heap):
-            if min_node > self.heap[index]:
-                min_node = self.heap[index]
-        return min_node
+        self.heap.append(value-1)
+        self.increase_value(len(self.heap) - 1, value)
 
     def heap_max(self):
         if not self.heap:
             return None
         return self.heap[0]
-
-    def heap_min(self):
-        if not self.heap:
-            return None
-        min_ind=self.heap.find_min()
-        min_node=self.heap[min_ind]
-        return min_node
 
     def extract_max(self):
         if not self.heap:
@@ -56,11 +43,48 @@ class PriorityQueueHeap:
         if len(self.heap) == 1:
             self.heap.pop()
             return max_node
-        # Sposta l'ultimo elemento in cima e rimuovi la radice
         self.heap[0] = self.heap[len(self.heap)-1]
         self.heap.pop(len(self.heap) - 1)
         self.max_heapify(self.heap[0])
         return max_node
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+    def min_heapify(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        if left < len(self.heap) and self.heap[left] > self.heap[index]:
+            max_node = left
+        else:
+            max_node = index
+        if right < len(self.heap) and self.heap[right] > self.heap[index]:
+            max_node = right
+        if max_node != index:
+            self.min_heapify(max_node)
+
+    def decrease_value(self, index, value):
+        if value > self.heap[index]:
+            raise ValueError(f"{value} is greater than {value}")
+        self.heap[index] = value
+        while index > 0 and self.heap[(index // 2) - 1] > self.heap[index]:
+            app = self.heap[(index // 2) - 1]
+            self.heap[index // 2 - 1] = self.heap[index]
+            self.heap[index] = app
+            index = index // 2 - 1
+
+    def insert(self, value):
+        self.heap.append(value +1)
+        self.decrease_value(len(self.heap) - 1, value)
+
+    def heap_min(self):
+        if not self.heap:
+            return None
+        return self.heap[0]
 
     def extract_min(self):
         if not self.heap:
@@ -69,9 +93,7 @@ class PriorityQueueHeap:
         if len(self.heap) == 1:
             self.heap.pop()
             return min_node
-        min_ind=self.heap.find_min()
-        min_node = self.heap[min_ind]
-        self.heap[min_ind]=self.heap[len(self.heap)-1]
+        self.heap[0] = self.heap[len(self.heap) - 1]
         self.heap.pop(len(self.heap) - 1)
-        self.max_heapify(min_ind)
+        self.min_heapify(self.heap[0])
         return min_node
