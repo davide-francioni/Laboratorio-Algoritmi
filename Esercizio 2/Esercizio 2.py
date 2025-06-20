4ab357295e8e16a338e8eaf193b4dae2b1b8691f
Davide Francioni <davide.francioni@edu.unifi.it>
2025-06-20 10:49:14 +0200

    esercizio 2 - test

--- Esercizio 2/Esercizio 2.py
+++ Esercizio 2/Esercizio 2.py
@@ -1,7 +1,7 @@
 import random
+import time
 
 def selection_sort(A):
-    #print("Lista iniziale Selection-sort", A)
     for i in range (0, len(A)):
         m=i
         for j in range(i, len(A)):
@@ -29,15 +29,71 @@ def partition(A,p,r):
 def genera_array_casuale(dimensione, minimo=0, massimo=100):
     return [random.randint(minimo, massimo) for _ in range(dimensione)]
 
+def funzione_da_misurare():
+    # Simuliamo un lavoro "pesante"
+    total = 0
+    for i in range(1000000):
+        total += i
+    return total
+
+def confrontig(n):
+    A = genera_array_casuale(n)
+    B = [0] * n
+    for i in range(0, n):
+        B[i] = A[i]
+
+    print("Lista iniziale Selection-sort", A)
+    print("Avvio Selection-sort...")
+    s_start_time = time.time()
+    selection_sort(A)
+    s_end_time = time.time()
+    s_tempo_impiegato = s_end_time - s_start_time
+    print("Lista finale Selection-sort", A)
+    print(f"Tempo impiegato: {s_tempo_impiegato:.6f} secondi")
+
+    print(" ")
+
+    print("Lista iniziale Quick-sort", B)
+    print("Avvio Quick-sort...")
+    q_start_time = time.time()
+    quick_sort(B, 0, len(B) - 1)
+    q_end_time = time.time()
+    q_tempo_impiegato = q_end_time - q_start_time
+    print("Lista finale Quick-sort", B)
+    print(f"Tempo impiegato: {q_tempo_impiegato:.6f} secondi")
+
+    print(" ")
+
+    if s_tempo_impiegato < q_tempo_impiegato:
+        print("Selection-sort più veloce di Quick-sort")
+    if s_tempo_impiegato > q_tempo_impiegato:
+        print("Quick-sort più veloce di Selection-sort")
+    if s_tempo_impiegato == q_tempo_impiegato:
+        print("Selection-sort veloce come Quick-sort")
 
 if __name__ == '__main__':
-    A=genera_array_casuale(10)
-    B=[0,0,0,0,0,0,0,0,0,0]
-    for i in range(0,10):
-        B[i]=A[i]
-    print("Lista iniziale Quick-sort", B)
+    print("Confrontiamo per 3 elementi:")
+    print(" ")
+    confrontig(3)
+    print(" ")
+    print("Confrontiamo per 5 elementi:")
+    print(" ")
+    confrontig(5)
+    print(" ")
+    print("Confrontiamo per 10 elementi:")
+    print(" ")
+    confrontig(10)
+    print(" ")
+    print("Confrontiamo per 20 elementi:")
+    print(" ")
+    confrontig(20)
+    print(" ")
+    print("Confrontiamo per 30 elementi:")
+    print(" ")
+    confrontig(30)
+    print(" ")
+    print("Confrontiamo per 100 elementi:")
+    print(" ")
+    confrontig(100)
+    print(" ")
 
-    selection_sort(A)
-    quick_sort(B, 0, len(B)-1)
-    #print("Lista finale Selection-sort", A)
-    print("Lista finale Quick-sort", B)
